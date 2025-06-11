10256312d96e5d836d28aed116b282518c841c6c
Clairton Lima <108296651+Clairtonlm@users.noreply.github.com>
2025-06-11 09:50:39 -0300

    projeto gestão de gastos pessoais

---
+++ gestao-custos/src/test/java/br/com/despesas/gestao_custos/GestaoCustosPessoaisApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.despesas.gestao_custos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaoCustosPessoaisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
